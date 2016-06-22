ae44300e5df8e4c33d4521bdd467038c50cbbfcd
Arslan <arslan.abdulkadyrov@gmail.com>
2016-06-22 05:33:42 +0300

    Картинки пропали

--- src/gui/BookingForm.java
+++ src/gui/BookingForm.java
@@ -41,24 +41,7 @@ public class BookingForm extends JFrame {
 
     private Map<String, ImageIcon> imageMap;
     String[][] nameList = new String[2][];
-//    {{"Angry birds", "Алиса в зазеркалье", "Варкрафт", "Выживший", "Иллюзия обмана 2",
-//            "Славные ублюдки","Стажер"},
-//            {"http://www.kinocenter.ru/upload/iblock/44d/44dfe0cfce64a9e960832e75c0e4a7b0.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/b8b/b8b767d6145205dbe35a6145443ccad3.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/76e/76effd735c8d4bcb3d778f8346807daa.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/19e/19e6cd3a4cb2a79406dfac7a048932d0.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/f0b/f0b8bdc16606b02cf12533188eef00fa.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/a3b/a3ba505dd2e1478943ca0267735d22a3.jpg",
-//                    "http://www.kinocenter.ru/upload/iblock/dfd/dfd10a8eec04e5c7f1dce5b125258eb6.jpg"
-//            }
-//    };
-
     String[] addresses = null;
-//    {"ул. Дмитрия Ульянова, 16",
-//            "Подкопаевский пер., 2/6",
-//            "Яузская ул., 11/6 строение 11",
-//            "ул. Пречистенка, 22/2",
-//            "Рогожский Вал ул., 7"};
 
     private void constructForm() {
         movies = dbWorker.getMovies();
