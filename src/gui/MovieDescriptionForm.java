1e5c991076ca7073d11dfe07300a3a43c7a127e7
Arslan <arslan.abdulkadyrov@gmail.com>
2016-06-22 17:34:31 +0300

    com

--- src/gui/MovieDescriptionForm.java
+++ src/gui/MovieDescriptionForm.java
@@ -2,6 +2,7 @@ package gui;
 
 import database.DBWorker;
 import database.Movies;
+import database.Sessions;
 import gui.BookingPlace;
 
 import javax.swing.*;
@@ -11,6 +12,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  * Created by Arslan on 21.06.2016.
@@ -22,17 +25,29 @@ public class MovieDescriptionForm extends JFrame {
     private JLabel label1;
     private JTextArea textArea1;
 
+    String[] listOfSessions = null;
+
 //    public static void main(String[] args) {
 //        new MovieDescriptionForm(null).setDefaultCloseOperation(EXIT_ON_CLOSE);
 //
 //    }
-    Movies movie = null;
+    private Movies movie = null;
+    private ArrayList<Sessions> sessions = null;
+
+    SimpleDateFormat localDateFormat = new SimpleDateFormat("HH:mm:ss");
 
     public MovieDescriptionForm(DBWorker dbWorker, Movies movie, JFrame parentJFrame) {
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int) (0.5 * dimension.getSize().width), (int) (0.7 * dimension.getSize().height));
 
-        list1.setListData(list);
+        sessions = dbWorker.getSessions(movie);
+        listOfSessions = new String[sessions.size()];
+        int i = 0;
+        for (Sessions session: sessions) {
+            listOfSessions[i] = localDateFormat.format(session.getDate()) +
+                    ", Зал " + session.getIdhall() + ", Цена" + session.getPrice();
+        }
+        list1.setListData(listOfSessions);
 
         this.movie = movie;
         try {
@@ -69,18 +84,17 @@ public class MovieDescriptionForm extends JFrame {
     private void invokeBookingPlace() {
         BookingPlace bookingPlace = new BookingPlace(null);
     }
-
-    String[] list = {
-            "9:00, Зал 458, ул. Дмитрия Ульянова, 16",
-            "12:00, Зал 2, Подкопаевский пер., 2/6",
-            "15:00, Зал 4, Яузская ул., 11/6 строение 11",
-            "15:00, Зал 5, ул. Дмитрия Ульянова, 16",
-            "17:00, Зал 458, ул. Дмитрия Ульянова, 16",
-            "19:00, Зал 2, Подкопаевский пер., 2/6",
-            "19:00, Зал 4, Яузская ул., 11/6 строение 11",
-            "21:00, Зал 5, ул. Дмитрия Ульянова, 16",
-            "23:00, Зал 458, ул. Дмитрия Ульянова, 16",
-            "23:00, Зал 2, Подкопаевский пер., 2/6",
-            "23:00, Зал 4, Яузская ул., 11/6 строение 11",
-            "23:00, Зал 5, ул. Дмитрия Ульянова, 16"};
+//     = {
+//            "9:00, Зал 458, ул. Дмитрия Ульянова, 16",
+//            "12:00, Зал 2, Подкопаевский пер., 2/6",
+//            "15:00, Зал 4, Яузская ул., 11/6 строение 11",
+//            "15:00, Зал 5, ул. Дмитрия Ульянова, 16",
+//            "17:00, Зал 458, ул. Дмитрия Ульянова, 16",
+//            "19:00, Зал 2, Подкопаевский пер., 2/6",
+//            "19:00, Зал 4, Яузская ул., 11/6 строение 11",
+//            "21:00, Зал 5, ул. Дмитрия Ульянова, 16",
+//            "23:00, Зал 458, ул. Дмитрия Ульянова, 16",
+//            "23:00, Зал 2, Подкопаевский пер., 2/6",
+//            "23:00, Зал 4, Яузская ул., 11/6 строение 11",
+//            "23:00, Зал 5, ул. Дмитрия Ульянова, 16"};
 }
